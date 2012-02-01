1f6e220f27b4ca20edbbff445e2af54c43b2c3c9
Jacques Uber <uberj@onid.orst.edu>
2012-02-01 10:19:19 -0800

    nameserver tests and models written. coverate at %100 for that module.

--- cydns/nameserver/models.py
+++ cydns/nameserver/models.py
@@ -1,12 +1,84 @@
 from django.db import models
-from cyder.cydns.domain.models import Domain
+from cyder.cydns.address_record.models import Domain, _check_TLD_condition
 from cyder.cydns.address_record.models import AddressRecord
+from cyder.cydns.models import _validate_label, _validate_name, InvalidRecordNameError, RecordExistsError
+import pdb
 
 class Nameserver( models.Model ):
     id              = models.AutoField(primary_key=True)
-    name            = models.CharField(max_length=100)
-    glue            = models.ForeignKey(AddressRecord, null=False)
     domain          = models.ForeignKey(Domain, null=False)
+    # "If the name server does lie within the domain it should have a corresponding A record."
+    server          = models.CharField(max_length=256)
+    glue            = models.ForeignKey(AddressRecord, null=True)
 
     class Meta:
         db_table = 'nameserver'
+
+    def __init__(self, *args, **kwargs):
+        super(Nameserver, self).__init__(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        super(Nameserver, self).delete(*args, **kwargs)
+
+    def clean( self ):
+        if type(self.server) not in (type(''), type(u'')):
+            raise InvalidRecordNameError("Error: name must be type str")
+
+        _validate_name( self.server )
+        _check_exist( self )
+        _check_TLD_condition( self )
+
+        needs_glue = _needs_glue( self )
+        if self.glue and self.glue.__fqdn__() != self.server:
+            if not needs_glue:
+                raise NSRecordMisconfiguredError("Error: %s does not need a glue record." % (self.__str__()))
+            else:
+                raise NSRecordMisconfiguredError("Error: %s needs a correct glue record." % (self.__str__()))
+        if not self.glue and needs_glue:
+            raise NSRecordMisconfiguredError("Error: %s needs a correct glue record." % (self.__str__()))
+
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(Nameserver, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return "%s %s %s" % ( self.domain.__str__(), 'NS', self.server )
+
+    def __repr__(self):
+        return "<NS Record '%s'>" % (self.__str__())
+
+class NSRecordMisconfiguredError(Exception):
+    """This exception is thrown when an attempt is made to create an NS record that requires a glue, but
+       a glue record is not found."""
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return self.__repr__()
+    def __repr__(self):
+        return self.msg
+
+def _needs_glue( ns ):
+    # Replace the domain portion of the server with "".
+    # if domain == foo.com and server == ns1.foo.com.
+    #       ns1.foo.com --> ns1
+    possible_label = ns.server.replace("."+ns.domain.name, "")
+    try:
+        _validate_label(possible_label)
+    except InvalidRecordNameError, e:
+        # It's not a valid label
+        return False
+    return True
+
+def _check_TLD_condition( ns ):
+    domain = Domain.objects.filter( name = ns.server )
+    if not domain:
+        return
+    else:
+        raise InvalidRecordNameError( "You cannot create a NS record that is the name of a domain." )
+
+def _check_exist( ns ):
+    exist = Nameserver.objects.filter( server = ns.server, domain = ns.domain )
+    for possible in exist:
+        if possible.pk != ns.pk:
+            raise RecordExistsError(possible.__str__()+" already exists.")
--- cydns/nameserver/tests.py
+++ cydns/nameserver/tests.py
@@ -7,10 +7,145 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+from cyder.cydns.domain.models import Domain, DomainExistsError, MasterDomainNotFoundError, DomainNotFoundError
+from cyder.cydns.address_record.models import AddressRecord,RecordExistsError, RecordNotFoundError
+from cyder.cydns.nameserver.models import Nameserver,NSRecordMisconfiguredError
+from cyder.cydns.reverse_domain.models import ReverseDomain
+from cyder.cydns.ip.models import Ip
+from cyder.cydns.models import RecordExistsError, InvalidRecordNameError
+import pdb
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class NSTests(TestCase):
+    def setUp(self):
+        self.r = Domain( name = "ru" )
+        self.r.save()
+        self.f_r = Domain( name = "foo.ru" )
+        self.f_r.save()
+        self.b_f_r = Domain( name = "bar.foo.ru" )
+        self.b_f_r.save()
+
+        self._128 = ReverseDomain( name = '128', ip_type= '4' )
+        self._128.save()
+
+    def do_add(self, domain, server, glue = False):
+        ns = Nameserver( domain = domain, server = server)
+        if glue:
+            ns.glue = glue
+        ns.save()
+        ret = Nameserver.objects.filter( domain = domain, server = server )
+        if glue:
+            fglue = AddressRecord.objects.get( pk = ns.glue.pk )
+            self.assertEqual( len(ret), 1 )
+        self.assertEqual( len(ret), 1 )
+        return ns
+
+    def test_add_TLD(self):
+        data = { 'domain':self.r , 'server':'bar.foo.ru' }
+        self.assertRaises( InvalidRecordNameError, self.do_add, **data )
+
+    def test_add_ns(self):
+        data = { 'domain':self.r , 'server':'ns2.moot.ru' }
+        self.do_add( **data )
+
+        data = { 'domain':self.r , 'server':u'ns3.moot.ru' }
+        self.do_add( **data )
+
+        data = { 'domain':self.b_f_r , 'server':'n1.moot.ru' }
+        self.do_add( **data )
+
+        data = { 'domain':self.b_f_r , 'server':'ns2.moot.ru' }
+        self.do_add( **data )
+
+        data = { 'domain':self.r , 'server':'asdf.asdf' }
+        self.do_add( **data )
+
+    def test_add_invalid(self):
+        data = { 'domain':self.f_r , 'server':'ns3.foo.ru' }
+        self.assertRaises( NSRecordMisconfiguredError, self.do_add, **data  )
+
+    def testtest_add_ns_in_domain(self):
+        ip = Ip( ip_str = '128.193.1.10', ip_type = '4' )
+        ip.save()
+        glue = AddressRecord( name='ns2', domain = self.r, ip = ip, ip_type='4' )
+        glue.save()
+        data = { 'domain':self.r , 'server':'ns2.ru', 'glue':glue }
+        ns = self.do_add( **data )
+        self.assertTrue( ns.glue )
+        self.assertEqual( ns.server, ns.glue.__fqdn__() )
+
+        ip = Ip( ip_str = '128.193.1.10', ip_type = '4' )
+        ip.save()
+        glue = AddressRecord( name='ns3', domain = self.f_r, ip = ip, ip_type='4' )
+        glue.save()
+        data = { 'domain':self.f_r , 'server':'ns3.foo.ru', 'glue':glue }
+        ns = self.do_add( **data )
+        self.assertTrue( ns.glue )
+        self.assertEqual( ns.server, ns.glue.__fqdn__() )
+
+    def test_add_ns_outside_domain(self):
+        data = { 'domain':self.f_r , 'server':'ns2.ru' }
+        ns = self.do_add( **data )
+        self.assertFalse( ns.glue )
+
+    def test_update_glue_to_no_glue(self):
+        ip = Ip( ip_str = '128.193.1.10', ip_type = '4' )
+        ip.save()
+        glue = AddressRecord( name='ns3', domain = self.r, ip = ip, ip_type='4' )
+        glue.save()
+        data = { 'domain':self.r , 'server':'ns3.ru', 'glue':glue }
+        ns = self.do_add( **data )
+        self.assertTrue( ns.glue )
+        self.assertEqual( ns.server, ns.glue.__fqdn__() )
+        ns.server = "ns3.foo.com"
+        ns.glue = None
+        ns.save()
+        self.assertFalse( ns.glue )
+
+        ns.server = "ns3.ru"
+        ns.glue = glue
+        ns.save()
+        self.assertTrue( ns.glue )
+        self.assertEqual( ns.server, ns.glue.__fqdn__() )
+
+        ns.server = "ns4.ru"
+        ns.glue = glue
+        self.assertRaises(NSRecordMisconfiguredError, ns.save )
+
+    def test_delete_ns(self):
+        ip = Ip( ip_str = '128.196.1.10', ip_type = '4' )
+        ip.save()
+        glue = AddressRecord( name='ns4', domain = self.f_r, ip = ip, ip_type='4' )
+        glue.save()
+        data = { 'domain':self.f_r , 'server':'ns4.foo.ru', 'glue':glue }
+        ns = self.do_add( **data )
+        self.assertTrue( ns.glue )
+        self.assertEqual( ns.server, ns.glue.__fqdn__() )
+
+        ns.delete()
+        nsret = Nameserver.objects.filter( server = 'ns2.foo.ru', domain = self.f_r )
+        self.assertFalse( nsret )
+
+    def test_invalid_create(self):
+        ip = Ip( ip_str = '128.193.1.10', ip_type = '4' )
+        ip.save()
+        glue = AddressRecord( name='ns2', domain = self.r, ip = ip, ip_type = '4' )
+        glue.save()
+
+        data = { 'domain':self.r , 'server':'ns2 .ru', 'glue':glue }
+        self.assertRaises( InvalidRecordNameError, self.do_add, **data )
+        data = { 'domain':self.r , 'server':'ns2$.ru', 'glue':glue }
+        self.assertRaises( InvalidRecordNameError, self.do_add, **data )
+        data = { 'domain':self.r , 'server':'ns2..ru', 'glue':glue }
+        self.assertRaises( InvalidRecordNameError, self.do_add, **data )
+        data = { 'domain':self.r , 'server':'ns2.ru ', 'glue':glue }
+        self.assertRaises( InvalidRecordNameError, self.do_add, **data )
+        data = { 'domain':self.r , 'server':True, 'glue':glue }
+        self.assertRaises( InvalidRecordNameError, self.do_add, **data )
+        data = { 'domain':self.r , 'server':'', 'glue':glue }
+        self.assertRaises( InvalidRecordNameError, self.do_add, **data )
+
+    def test_add_dup(self):
+        data = { 'domain':self.r , 'server':'ns2.moot.ru' }
+        self.do_add( **data )
+
+        self.assertRaises( RecordExistsError, self.do_add, **data)
