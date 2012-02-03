7fd88bff957f5b5aa800dd1302a9a92786181d00
Jacques Uber <uberj@onid.orst.edu>
2012-02-03 10:26:55 -0800

    initial mx models and tests written. %100 test coverage.

--- cydns/mx/models.py
+++ cydns/mx/models.py
@@ -1,13 +1,65 @@
 from django.db import models
 from cyder.cydns.domain.models import Domain
+from cyder.cydns.cydns import CommonRecord
+from cyder.cydns.models import InvalidRecordNameError, RecordExistsError, _validate_name, _validate_label, _validate_ttl
 
-class MX( models.Model ):
+class MX( CommonRecord ):
     id              = models.AutoField(primary_key=True)
-    name            = models.CharField(max_length=100)
+    # The mail server this record should point to.
     server          = models.CharField(max_length=100)
-    domain          = models.ForeignKey(Domain, null=False)
     priority        = models.PositiveIntegerField(null=False)
     ttl             = models.PositiveIntegerField(null=False)
 
+    def delete(self, *args, **kwargs):
+        super(MX, self).delete(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(MX, self).save(*args, **kwargs)
+
+    def clean( self ):
+        if type(self.label) not in (type(''), type(u'')):
+            raise InvalidRecordNameError("Error: name must be type str")
+        if type(self.server) not in (type(''), type(u'')):
+            raise InvalidRecordNameError("Error: name must be type str")
+        _validate_label( self.label )
+        _validate_name( self.server )
+        _validate_mx_priority( self.priority )
+        _validate_ttl( self.ttl )
+        _check_exists( self )
+        _check_TLD_condition( self )
+
+    def __str__(self):
+        return "%s %s %s %s %s %s" % ( self.__fqdn__(), self.ttl, 'IN', 'MX', self.priority, self.server)
+
+    def __fqdn__(self):
+        if self.label == '':
+            fqdn = self.domain.name
+        else:
+            fqdn = str(self.label)+"."+self.domain.name
+        return fqdn
+
+    def __repr__(self):
+        return "<MX '%s'>" % (self.__str__())
+
     class Meta:
         db_table = 'mx'
+
+def _validate_mx_priority( prio ):
+    if prio > 65535 or prio < 0:
+        raise InvalidRecordNameError("Error: MX priority must be within the 0 to 65535 range. See RFC 1035")
+
+def _check_exists( mx ):
+    exist = MX.objects.filter( label = mx.label, server = mx.server, priority = mx.priority, ttl = mx.ttl, domain = mx.domain )
+    for possible in exist:
+        if possible.pk != mx.pk:
+            raise RecordExistsError("Error: This MX record already exists.")
+
+def _check_TLD_condition( mx ):
+    domain = Domain.objects.filter( name = mx.__fqdn__() )
+    if not domain:
+        return
+    if mx.label == '' and domain[0] == mx.domain:
+        return #This is allowed
+    else:
+        raise InvalidRecordNameError( "You cannot create a MX record with a non empty label that points to a TLD." )
--- cydns/mx/tests.py
+++ cydns/mx/tests.py
@@ -6,11 +6,87 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from cyder.cydns.models import RecordExistsError, InvalidRecordNameError
+from cyder.cydns.mx.models import MX
+from cyder.cydns.domain.models import Domain
 
+import pdb
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class MXTests(TestCase):
+
+    def setUp(self):
+        self.o = Domain( name = "org" )
+        self.o.save()
+        self.o_e = Domain( name = "oregonstate.org")
+        self.o_e.save()
+        self.b_o_e = Domain( name = "bar.oregonstate.org")
+        self.b_o_e.save()
+
+    def do_generic_add(self, data ):
+        mx = MX( **data )
+        mx.__repr__()
+        mx.save()
+        rmx = MX.objects.filter( **data )
+        self.assertTrue( len(rmx) == 1 )
+        return mx
+
+    def test_add_mx(self):
+        data = { 'label':'' ,'domain':self.o_e ,'server':'relay.oregonstate.edu' ,'priority':2 ,'ttl':2222 }
+        self.do_generic_add( data )
+        data = { 'label':'' ,'domain':self.o_e ,'server':'mail.sdf.fo' ,'priority':9 ,'ttl':34234 }
+        self.do_generic_add( data )
+        data = { 'label':'mail' ,'domain':self.b_o_e ,'server':'asdf.asdf' ,'priority':123 ,'ttl':213 }
+        self.do_generic_add( data )
+        data = { 'label':'' ,'domain':self.b_o_e ,'server':'oregonstate.edu' ,'priority':2 ,'ttl':2 }
+        self.do_generic_add( data )
+        data = { 'label':u'dsfasdfasdfasdfasdfasdfasdf' ,'domain':self.o ,'server':'nope.mail' ,'priority':12 ,'ttl':124 }
+        self.do_generic_add( data )
+
+    def test_add_invalid(self):
+        data = { 'label': 'bar','domain':self.o_e,'server':'mail.oregonstate.edu','priority':123,'ttl':23}
+        self.assertRaises(InvalidRecordNameError, self.do_generic_add, data ) # TLD condition
+        data = { 'label':'adsf,com' ,'domain':self.o_e ,'server':'mail.oregonstate.edu' ,'priority':123 ,'ttl':23 }
+        self.assertRaises(InvalidRecordNameError, self.do_generic_add, data )
+        data = { 'label':'foo' ,'domain':self.o_e ,'server':'mail..com' ,'priority':34 ,'ttl':1234 }
+        self.assertRaises(InvalidRecordNameError, self.do_generic_add, data )
+        data = { 'label':'foo.bar' ,'domain':self.o_e ,'server':'mail.com' ,'priority':3 ,'ttl':23424 }
+        self.assertRaises(InvalidRecordNameError, self.do_generic_add, data )
+        data = { 'label':123 ,'domain':self.o_e ,'server':'sdf' ,'priority':2 ,'ttl':32 }
+        self.assertRaises(InvalidRecordNameError, self.do_generic_add, data )
+        data = { 'label':"asdf#$@" ,'domain':self.o_e ,'server':'coo.com' ,'priority':123 ,'ttl':23 }
+        self.assertRaises(InvalidRecordNameError, self.do_generic_add, data )
+        data = { 'label':"asdf" ,'domain':self.o_e ,'server':'coo.com' ,'priority':-1 ,'ttl':23 }
+        self.assertRaises(InvalidRecordNameError, self.do_generic_add, data )
+        data = { 'label':"asdf" ,'domain':self.o_e ,'server':'coo.com' ,'priority':65536 ,'ttl':23 }
+        self.assertRaises(InvalidRecordNameError, self.do_generic_add, data )
+
+    def do_remove(self, data):
+        mx = self.do_generic_add( data )
+        mx.delete()
+        rmx = MX.objects.filter( **data )
+        self.assertTrue( len(rmx) == 0 )
+
+    def test_remove(self):
+        data = { 'label':'' ,'domain':self.o_e ,'server':'frelay.oregonstate.edu' ,'priority':2 ,'ttl':2222 }
+        self.do_remove( data )
+        data = { 'label':'' ,'domain':self.o_e ,'server':'fmail.sdf.fo' ,'priority':9 ,'ttl':34234 }
+        self.do_remove( data )
+        data = { 'label':'mail' ,'domain':self.b_o_e ,'server':'asdff.asdf' ,'priority':123 ,'ttl':213 }
+        self.do_remove( data )
+        data = { 'label':'' ,'domain':self.b_o_e ,'server':'oregonsftate.edu' ,'priority':2 ,'ttl':2 }
+        self.do_remove( data )
+        data = { 'label':u'dsfasdfasdfasdfasdfasdfasdf' ,'domain':self.o ,'server':'nopef.mail' ,'priority':12 ,'ttl':124 }
+        self.do_remove( data )
+
+    def test_add_and_update_dup(self):
+        data = { 'label':'' ,'domain':self.o_e ,'server':'relaydf.oregonstate.edu' ,'priority':2 ,'ttl':2222 }
+        mx0 = self.do_generic_add( data )
+        self.assertRaises( RecordExistsError, self.do_generic_add, data )
+        data = { 'label':'' ,'domain':self.o_e ,'server':'mail.sddf.fo' ,'priority':9 ,'ttl':34234 }
+        mx1 = self.do_generic_add( data )
+        self.assertRaises( RecordExistsError, self.do_generic_add, data )
+
+        mx0.server = "mail.sddf.fo"
+        mx0.priority = 9
+        mx0.ttl = 34234
+        self.assertRaises( RecordExistsError, mx0.save )
