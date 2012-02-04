dd2ebdd230cfe0a46cdf747605d28380fd1948be
Jacques Uber <uberj@onid.orst.edu>
2012-02-03 16:36:30 -0800

    wrote txt model.

--- cydns/txt/models.py
+++ cydns/txt/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from cyder.cydns.domain.models import Domain
+from cyder.cydns.cydns import CommonRecord
 
-class TXT( models.Model ):
+class TXT( CommonRecord ):
     id              = models.AutoField(primary_key=True)
-    name            = models.CharField(max_length=100)
     meta_data       = models.TextField()
-    domain          = models.ForeignKey(Domain, null=False)
 
     class Meta:
         db_table = 'txt'
