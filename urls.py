9d5910ad399a7ba72d383c5c12f01141d77f4919
Jacques Uber <uberj@onid.orst.edu>
2012-02-09 16:26:15 -0800

    added reverse domain views.

--- urls.py
+++ urls.py
@@ -5,9 +5,20 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cyder.views.home', name='home'),
-    # url(r'^cyder/', include('cyder.foo.urls')),
+
+    #########
+    # CYDNS #
+    #########
+    # Domain
+    url(r'^cyder/cydns/domain$', 'cyder.cydns.domain.views.domain_list'),
+    url(r'^cyder/cydns/domain/create$', 'cyder.cydns.domain.views.domain_create'),
+    url(r'^cyder/cydns/domain/(?P<pk>[\w-]+)/update$', 'cyder.cydns.domain.views.domain_update'),
+
+    # Reverse Domain
+    url(r'^cyder/cydns/reverse_domain$', 'cyder.cydns.reverse_domain.views.reverse_domain_list'),
+    url(r'^cyder/cydns/reverse_domain/create$', 'cyder.cydns.reverse_domain.views.reverse_domain_create'),
+    url(r'^cyder/cydns/reverse_domain/(?P<pk>[\w-]+)/update$', 'cyder.cydns.reverse_domain.views.reverse_domain_update'),
+    url(r'^cyder/cydns/reverse_domain/bootstrap6$', 'cyder.cydns.reverse_domain.views.bootstrap_ipv6'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
