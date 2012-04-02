1786c4400a12784babadc78c6159cb38b77d51df
uberj <uberj@onid.orst.edu>
2012-04-02 16:54:25 -0700

    started to sketch out the registrations stuff.

--- cyder/core/registrations/models.py
+++ cyder/core/registrations/models.py
@@ -5,7 +5,7 @@ from cyder.cydhcp.range.models import Range
 from cyder.core.container.models import CTNR
 from cyder.core.node.models import Node
 
-class Static_Registration( models.Model ):
+class StaticRegistration( models.Model ):
     id              = models.AutoField(primary_key=True)
     A               = models.OneToOneField(AddressRecord)
     PTR             = models.OneToOneField(PTR)
@@ -16,7 +16,7 @@ class Static_Registration( models.Model ):
     class Meta:
         db_table = 'static_registration'
 
-class Dynamic_Registration( models.Model ):
+class DynamicRegistration( models.Model ):
     id              = models.AutoField(primary_key=True)
     range           = models.ForeignKey(Range, null=False)
     mac             = models.CharField(max_length=12) # TODO make a mac table?
