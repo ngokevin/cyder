9fdf46b94cf03d79fcb1273d8fb9be940d93e5bc
Kevin Ngo <kevin.ngo@oregonstate.edu>
2012-04-06 02:29:41 -0700

    fixed r_ns urls

--- cyder/cydns/nameserver/reverse_nameserver/urls.py
+++ cyder/cydns/nameserver/reverse_nameserver/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import *
 
+from cyder.cydns.nameserver.reverse_nameserver.views import *
+
 urlpatterns = patterns('',
-    url(r'reverse_nameserver/$', RevNSListView.as_view()),
-    url(r'reverse_nameserver/create/$', RevNSCreateView.as_view()),
-    url(r'reverse_nameserver/(?P<pk>[\w-]+)/$', RevNSDetailView.as_view()),
-    url(r'reverse_nameserver/(?P<reverse_domain_pk>[\w-]+)/create/$', RevNSCreateView.as_view()),
-    url(r'reverse_nameserver/(?P<pk>[\w-]+)/update/$', RevNSUpdateView.as_view()),
-    url(r'reverse_nameserver/(?P<pk>[\w-]+)/delete/$', RevNSDeleteView.as_view()),
+    url(r'^$', RevNSListView.as_view()),
+    url(r'create/$', RevNSCreateView.as_view()),
+    url(r'(?P<pk>[\w-]+)/$', RevNSDetailView.as_view()),
+    url(r'(?P<reverse_domain_pk>[\w-]+)/create/$', RevNSCreateView.as_view()),
+    url(r'(?P<pk>[\w-]+)/update/$', RevNSUpdateView.as_view()),
+    url(r'(?P<pk>[\w-]+)/delete/$', RevNSDeleteView.as_view()),
 )
