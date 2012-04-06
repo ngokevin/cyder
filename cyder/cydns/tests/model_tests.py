54fbffa4568a29978e8faf162791143c7875894b
uberj <uberj@onid.orst.edu>
2012-04-06 12:08:09 -0700

    fixed test import.

--- cyder/cydns/tests/model_tests.py
+++ cyder/cydns/tests/model_tests.py
@@ -9,7 +9,7 @@ from cyder.cydns.address_record.tests import *
 from cyder.cydns.domain.tests import *
 from cyder.cydns.ip.tests import *
 from cyder.cydns.nameserver.tests import *
-from cyder.cydns.nameserver.reverse_tests import *
+from cyder.cydns.nameserver.reverse_nameserver.tests import *
 from cyder.cydns.ptr.tests import *
 from cyder.cydns.reverse_domain.tests import *
 from cyder.cydns.soa.tests import *
