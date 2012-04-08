688f25609229582803258cea3a7da04fcd823eaa
Kevin Ngo <kevin.ngo@oregonstate.edu>
2012-04-08 14:52:00 -0700

    fix ctnr urls to allow ctnr switching, fix ctnr detail template

--- cyder/core/ctnr/urls.py
+++ cyder/core/ctnr/urls.py
@@ -7,7 +7,6 @@ urlpatterns = patterns('cyder.core.ctnr.views',
     url(r'create/$', CtnrCreateView.as_view()),
     url(r'(?P<pk>[\w-]+)/update/$', CtnrUpdateView.as_view() ),
     url(r'(?P<pk>[\w-]+)/delete/$', CtnrDeleteView.as_view() ),
-    url(r'(?P<pk>[\w-]+)/$', CtnrDetailView.as_view() ),
-
     url(r'(?P<pk>[\w-]+)?/?change/$', 'change_ctnr'),
+    url(r'(?P<pk>[\w-]+)/$', CtnrDetailView.as_view() ),
 )
